a0026ddfdba2e40e6a9f7e7cfccc2aa969d9bdf5
Tyler Sutton <309jtsutton@gmail.com>
2018-12-26 19:41:08 -0600

    Created Wrapper in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <Wrapper>
+
+        <Landing />
+
+      </Wrapper>
     );
   }
 }
 
 export default App;
+
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100%;
+
+  position: absolute;
+
+  top: 0;
+  left: 0;
+
+  border: 2px solid red;
+`;
