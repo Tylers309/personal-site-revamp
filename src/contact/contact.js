7479090d495a506b0b73704aca1339af920835f4
Tyler Sutton <309jtsutton@gmail.com>
2019-03-12 08:38:17 -0500

    Add netlify

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -17,7 +17,7 @@ class Contact extends Component {
       <Wrapper>
         <WrapperHolder>
           <WrapperLeft>
-            <form name="contact" netlify>
+            <form name="contact" method="POST" data-netlify="true">
               <Header>Contact Me</Header>
             <Input type="text" name="name" placeholder="Name" />
             <Input type="email" name="email" placeholder="Email" />
